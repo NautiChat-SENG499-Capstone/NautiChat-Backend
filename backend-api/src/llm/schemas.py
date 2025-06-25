0e89eeec80efc5d40bd51bc3c166d831c956dfbd
Jack Klobchar <jackklobchar@gmail.com>
2025-06-24 18:23:47 -0700

    remove character limits on the message schema

--- backend-api/src/llm/schemas.py
+++ backend-api/src/llm/schemas.py
@@ -4,25 +4,28 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class Feedback(BaseModel):
     """User Feedback on LLM Response"""
+
     rating: Annotated[int, Field(strict=True, ge=1, le=5)]
     comment: Optional[str] = None
 
 
 class Message(BaseModel):
     """Single Interaction between user and LLM"""
+
     # Allows model to be populated from SQLAlchemy ORM objects
     model_config = ConfigDict(from_attributes=True)
 
     message_id: int
     conversation_id: int
     user_id: int
-    input: Annotated[str, Field(min_length=1, max_length=1000)]
-    response: Annotated[str, Field(min_length=1, max_length=1000)]
+    input: str
+    response: str
     feedback: Optional[Feedback] = None
 
 
 class Conversation(BaseModel):
     """Conversation Thread b/w user and LLM"""
+
     model_config = ConfigDict(from_attributes=True)
 
     conversation_id: int
@@ -33,10 +36,12 @@ class Conversation(BaseModel):
 
 class CreateConversationBody(BaseModel):
     """Payload for creating new conversation"""
+
     title: Optional[str] = None
 
 
 class CreateLLMQuery(BaseModel):
     """Payload sent when querying LLM"""
+
     input: Annotated[str, Field(min_length=1, max_length=1000)]
     conversation_id: int
