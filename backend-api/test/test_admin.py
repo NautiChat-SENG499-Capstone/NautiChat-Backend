1f196fe70054f6c3a8d9a98f89281090350754b6
Paisley Lewis <paisley5@outlook.com>
2025-07-08 17:02:54 -0700

    Remove bad test

--- backend-api/test/test_admin.py
+++ backend-api/test/test_admin.py
@@ -25,18 +25,3 @@ async def test_clustered_messages_returns_valid_json(client: AsyncClient, admin_
     clusters = response.json()
     assert isinstance(clusters, dict)
     
-@pytest.mark.asyncio
-async def test_raw_text_upload(client: AsyncClient, user_headers):
-    payload = {
-        "source": "unit-test-source",
-        "information": "This is some test information to upload to the vector DB."
-    }
-
-    response = await client.post(
-        "/admin/data-upload",
-        headers=user_headers,
-        json=payload
-
-    )
-
-    assert response.status_code == status.HTTP_403_FORBIDDEN
